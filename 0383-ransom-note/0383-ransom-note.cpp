d368d78dca0f8b3d31714e78655c620a8ce689e6
AnanjayGoyal <ajgoyal1495@gmail.com>
2026-08-24 15:48:30 +0530

    Time: 2 ms (58.38%), Space: 11.9 MB (29.93%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+
+        unordered_map<char, int> mp;
+
+        // Store frequency of magazine
+        for(char c : magazine) {
+            mp[c]++;
+        }
+
+        // Build ransomNote from the letters of magazine
+        for(char c : ransomNote) {
+            auto it = mp.find(c);
+
+            if(it == mp.end() || it->second == 0) {
+                return false;
+            }
+            it->second--;
+        }
+        return true;
+    }
+};
