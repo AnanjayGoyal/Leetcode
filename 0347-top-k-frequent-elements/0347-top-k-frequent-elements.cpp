a8731d493bbdd5856bda504ab5ab07bcfdafae19
AnanjayGoyal <ajgoyal1495@gmail.com>
2026-08-29 12:05:53 +0530

    Time: 5 ms (23.64%), Space: 17.9 MB (72.37%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,3 +1,5 @@
+// For Frequent elements, we will use HashMaps.
+// For Top k elements, we use min Heap.
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
@@ -6,14 +8,17 @@ public:
         for (int x : nums)
             freq[x]++;
 
+        // Min Heap
         priority_queue<
             pair<int, int>,
             vector<pair<int, int>>,
             greater<pair<int, int>>> pq;
 
-        for (auto [num, count] : freq) {
-            pq.push({count, num});
+        for (auto it : freq) {
+            // We have to minimise the frequency (Push it.second)
+            pq.push({it.second, it.first});    
 
+            // Keeping top k elements in the heap
             if (pq.size() > k)
                 pq.pop();
         }
