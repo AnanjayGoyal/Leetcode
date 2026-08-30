3fbe8ed241d70cd33c600eaeb05f92ac9e092c90
AnanjayGoyal <ajgoyal1495@gmail.com>
2026-08-30 14:39:26 +0530

    Time: 4 ms (63.53%), Space: 14.7 MB (8.46%) - LeetHub

--- 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.cpp
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.cpp
@@ -1,44 +1,29 @@
-// While iterating, we need to remember the element, so we use hahsmaps.
+// We need to remember the current number while itearating all the numbers to check which are smaller, so we will use hashmaps. hashmap will store "number:smaller_numbers" as key:value pairs.
+// sort the vector then insert it into the hashmap, the index of each element in the sorted vetor is the smaller_numbers for that number, so store number:index in hahsmap
 class Solution {
 public:
     vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
+        vector<int> v = nums;
 
-        unordered_map<int, int> mp;
-
-        // Count frequency
-        for (int x : nums) {
-            mp[x]++;
-        }
-
-        // Put numbers into vector
-        vector<int> v;
-
-        for (auto it : mp) {
-            v.push_back(it.first);
-        }
-
-        // Sort numbers
+        // Sort
         sort(v.begin(), v.end());
 
-        // Store answer for each number
-        unordered_map<int, int> smaller;
+        // Store first index of every number
+        unordered_map<int, int> mp;
 
-        int count = 0;
+        for (int i = 0; i < v.size(); i++) {
 
-        for (int x : v) {
-
-            smaller[x] = count;
-
-            count += mp[x];
+            if (mp.find(v[i]) == mp.end()) {
+                mp[v[i]] = i;
+            }
         }
 
-        // Build final answer
+        // Build answer
         vector<int> ans;
 
         for (int x : nums) {
-            ans.push_back(smaller[x]);
+            ans.push_back(mp[x]);
         }
-
         return ans;
     }
 };
