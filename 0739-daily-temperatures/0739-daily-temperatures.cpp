c680b38aa87a0a5d912b482528875c5e9c4e284d
AnanjayGoyal <ajgoyal1495@gmail.com>
2026-09-03 18:30:17 +0530

    Time: 24 ms (45.2%), Space: 107.3 MB (47.37%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -0,0 +1,21 @@
+// Just instead of storing the next greater element for every element, store the index difference.
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+
+        int n = temperatures.size();
+        vector<int> ans(n, 0);
+        stack<int> st;
+
+        for (int i = 0; i < n; i++) {
+
+            while (!st.empty() && temperatures[st.top()] < temperatures[i]){
+
+                ans[st.top()] = i - st.top();
+                st.pop();
+            }
+            st.push(i);
+        }
+        return ans;
+    }
+};
