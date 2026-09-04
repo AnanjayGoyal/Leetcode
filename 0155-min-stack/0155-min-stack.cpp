b88b89c01489768b2179a8b0b753777ae01083e7
AnanjayGoyal <ajgoyal1495@gmail.com>
2026-09-04 19:15:07 +0530

    Time: 50 ms (80.53%), Space: 151.3 MB (37.76%) - LeetHub

---
+++ 0155-min-stack/0155-min-stack.cpp
@@ -0,0 +1,31 @@
+// Keep the track of minimum elements in real time when you are pushing and popping out.
+class MinStack {
+public:
+    stack<int> st;
+    stack<int> minSt;
+
+    MinStack() {
+    }
+    
+    void push(int value) {
+        st.push(value);
+        if (minSt.empty() || value <= minSt.top()) {
+            minSt.push(value);
+        }
+    }
+    
+    void pop() {
+        if (st.top() == minSt.top()) {
+            minSt.pop();
+        }
+        st.pop();
+    }
+    
+    int top() {
+        return st.top();
+    }
+    
+    int getMin() {
+        return minSt.top();
+    }
+};
