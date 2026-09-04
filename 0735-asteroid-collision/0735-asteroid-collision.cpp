66e3e451accc66860c15d1809e58810c31206107
AnanjayGoyal <ajgoyal1495@gmail.com>
2026-09-04 14:53:02 +0530

    Time: 3 ms (27.94%), Space: 22.2 MB (17.92%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+
+        stack<int> st;
+
+        for (int curr : asteroids) {
+
+            bool alive = true;
+
+            while (alive && !st.empty() &&
+                   st.top() > 0 && curr < 0) {
+
+                // Stack top is smaller
+                if (st.top() < -curr) {
+                    st.pop();
+                }
+
+                // Both are same size
+                else if (st.top() == -curr) {
+                    st.pop();
+                    alive = false;
+                }
+
+                // Current is smaller
+                else {
+                    alive = false;
+                }
+            }
+
+            // Current asteroid survived
+            if (alive) {
+                st.push(curr);
+            }
+        }
+
+        vector<int> ans;
+
+        while (!st.empty()) {
+            ans.push_back(st.top());
+            st.pop();
+        }
+
+        reverse(ans.begin(), ans.end());
+
+        return ans;
+    }
+};
