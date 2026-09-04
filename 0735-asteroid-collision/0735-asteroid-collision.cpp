8a2d61cd32a0f7556f04b1953fbf4de2f954b44e
AnanjayGoyal <ajgoyal1495@gmail.com>
2026-09-04 14:54:34 +0530

    Time: 0 ms (100%), Space: 22.2 MB (17.92%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -1,48 +1,39 @@
 class Solution {
 public:
     vector<int> asteroidCollision(vector<int>& asteroids) {
-
         stack<int> st;
 
         for (int curr : asteroids) {
-
             bool alive = true;
 
-            while (alive && !st.empty() &&
-                   st.top() > 0 && curr < 0) {
-
+            // Checking if the Collision is Possible.
+            while (alive && !st.empty() && st.top() > 0 && curr < 0) {
                 // Stack top is smaller
                 if (st.top() < -curr) {
                     st.pop();
                 }
-
                 // Both are same size
                 else if (st.top() == -curr) {
                     st.pop();
                     alive = false;
                 }
-
                 // Current is smaller
                 else {
                     alive = false;
                 }
             }
-
             // Current asteroid survived
             if (alive) {
                 st.push(curr);
             }
         }
-
+        
         vector<int> ans;
-
         while (!st.empty()) {
             ans.push_back(st.top());
             st.pop();
         }
-
         reverse(ans.begin(), ans.end());
-
         return ans;
     }
 };
