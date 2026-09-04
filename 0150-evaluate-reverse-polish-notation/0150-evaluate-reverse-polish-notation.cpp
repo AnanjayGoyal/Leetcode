ddd3b6e4aeb28ea190d5c0048e60956f85a9cc42
AnanjayGoyal <ajgoyal1495@gmail.com>
2026-09-04 18:28:02 +0530

    Time: 4 ms (19.43%), Space: 17.1 MB (66.97%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -1,11 +1,12 @@
+// [2, 3, +, 4, *] --> push 2 --> push 3 --> + appears so add and remove 2, 3 from stacks and push 6 to stack --> push 4, now stack contains [6, 4] --> * appears so push 24 to stack ---> Final result is 24.
+
 class Solution {
 public:
     int evalRPN(vector<string>& tokens) {
         stack<int> st;
         for (string curr : tokens) {
             // Number
-            if (curr != "+" && curr != "-" &&
-                curr != "*" && curr != "/") {
+            if (curr != "+" && curr != "-" && curr != "*" && curr != "/") {
                 st.push(stoi(curr));
             }
             // Operator
@@ -15,6 +16,7 @@ public:
                 int a = st.top();
                 st.pop();
                 int result;
+                
                 if (curr == "+")
                     result = a + b;
                 else if (curr == "-")
@@ -23,7 +25,7 @@ public:
                     result = a * b;
                 else
                     result = a / b;
-                    
+
                 st.push(result);
             }
         }
